e7c24a9972686a006fb5e672413a5690fca0d1dd
Sebastian <sebas513g@gmail.com>
2025-01-22 18:17:51 -0500

    TESTING headers

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,10 +65,19 @@
 
  form.addEventListener("submit", function(e){
     e.preventDefault()
+    let headers = new Headers()
+
+    headers.append('Content-Type', 'application/json')
+    headers.append('Accept', 'application/json')  
+    headers.append('Access-Control-Allow-Origin', 'http://floatmonitor.com')
+    headers.append('GET', 'POST');
     let searchInput = document.getElementById("ticker_search").value
     let url = "https://zk23ibi8il.execute-api.us-east-1.amazonaws.com/DEV/?ticker=" + searchInput
 
-    fetch(url).then(data => {
+    fetch(url, {
+      headers: headers, 
+      method: 'POST'
+    }).then(data => {
           console.log(data)
     })
 
